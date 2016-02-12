f0e3617a40a908b06ea0b577c6add69da5f2a4c0
Klaus Kuehnhammer <klaus@bitstem.com>
2016-02-12 15:18:29 +0100

    Initial Commit

---
+++ zendesk-integration-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zendesk-integration-test
+//
+//  Created by Klaus Kuehnhammer on 12/02/16.
+//  Copyright © 2016 Bitstem GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zendesk-integration-test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  zendesk-integration-test
+//
+//  Created by Klaus Kuehnhammer on 12/02/16.
+//  Copyright © 2016 Bitstem GmbH. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
